b2e21522c201edbe51219bdada36c47b19203da7
Tonatiuh González <martiuh>
2018-11-14 00:09:53 -0600

    prepare for pages router

--- src/pages/msg.js
+++ src/pages/msg.js
@@ -1,23 +1,85 @@
 import React from 'react'
 import { Router, Link } from '@reach/router'
+import axios from 'axios'
 
 import '../css/msg'
 
-function Msg() {
-  return (
-    <main>
-      <h1>Msg.js</h1>
-    </main>
-  )
+const CommentCard = ({ email, body, to }) => (
+  <React.Fragment>
+    <h2 className='msg-email'>{email}</h2>
+    <p className='msg-body'>{body}</p>
+    {to && <Link className='msg-link' to={to}>Ver más</Link>}
+  </React.Fragment>
+)
+
+CommentCard.defaultProps = {
+  email: '',
+  body: '',
+  to: ''
 }
 
-function MyCustomMessage({ message }) {
-  return (
-    <main>
-      {message && !IS_SERVER && <h3>{message}</h3>}
-      <Link to='/msg'>Back {`<--`}</Link>
-    </main>
-  )
+class Msg extends React.Component {
+  state = {
+    comments: [1,2,3,4,5,6,7,8,9,10],
+    fetched: null
+  }
+  
+  componentDidMount() {
+    axios.get('https://jsonplaceholder.typicode.com/comments')
+      .then(({ data }) => setTimeout(() => this.setState({ comments: data.slice(0, 10), fetched: true }), 500))
+  }
+
+  render() {
+    const { comments } = this.state
+    return (
+      <main>
+        <h1>Msg.js</h1>
+        {comments.map(C => {
+          if (typeof C === 'number') {
+            return <CommentCard key={C} />
+          }
+
+          return (
+            <CommentCard
+              key={C.id}
+              to={`/msg/${C.id}`}
+              body={C.body}
+              email={C.email}
+            />
+          )
+        })}
+      </main>
+    )
+  }
+}
+
+class MyCustomMessage extends React.Component {
+  state = {
+    comment: {},
+    fetched: false
+  }
+
+  componentDidMount() {
+    const { message } = this.props
+    if (typeof parseInt(message) === 'number') {
+      axios.get(`https://jsonplaceholder.typicode.com/comments/${message}`)
+        .then(({ data }) => {
+          this.setState({ comment: data, fetched: true })
+        })
+    }
+  }
+
+  render() {
+    const { comment, fetched } = this.state 
+    return (
+      <main>
+        {!fetched ? <CommentCard /> : (
+          <CommentCard key={comment.id} {...comment} />
+        )}
+        <Link to='/msg'>Back {`<--`}</Link>
+      </main>
+    )
+  }
 }
 
 export default () => (
@@ -26,8 +88,8 @@ export default () => (
       <Link to='/'>
         Inicio
       </Link>
-      <Link to='/msg/somos-chivas'>
-        Somos Chivas
+      <Link to='/msg/1'>
+        First Comment
       </Link>
     </nav>
     <Router>
